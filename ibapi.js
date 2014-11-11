54ae3fc893837078b45c75366c4477f2ab34be81
amitmiran <amitmiran@gmail.com>
2014-11-11 12:53:03 +0200

    time ordered messages arrival.  consumeMessages only after last consuming ended at client side  in that way we're controlling the messages in a real timeline like way

--- ibapi.js
+++ ibapi.js
@@ -1,234 +1,126 @@
 // module for event driven methods
-var addon = require('./build/Release/NodeIbapiAddon');
-var contract = require('./lib/contract');
-var order = require('./lib/order');
-var execution = require('./lib/execution');
-var contractDetails = require('./lib/contractDetails');
-var scannerSubscription = require('./lib/scannerSubscription');
-var events = require ("events");
-var util = require ("util");
+var addon = require('./build/Release/NodeIbapiAddon'),
+    contract = require('./lib/contract'),
+    order = require('./lib/order'),
+    execution = require('./lib/execution'),
+    contractDetails = require('./lib/contractDetails'),
+    scannerSubscription = require('./lib/scannerSubscription'),
+    messageIds = require('./messageIds'),
+    async = require("async");
 
-for (var key in events.EventEmitter.prototype) {
-  addon.NodeIbapi.prototype[key] = events.EventEmitter.prototype[key];
+function NodeIBApi() {
+    this.client = addon.NodeIbapi();
+    this.handlers = {};
+
+    this._messageQueue = async.queue(this._consumeMessage, 1);
+
+    this._messageQueue.pause();
 }
 
-addon.NodeIbapi.prototype.funcQueue = [];
+NodeIBApi.prototype = {
 
-addon.NodeIbapi.prototype.addReqId = function () {
-  this.reqIds(1);
-}
+    _consumeMessage: function (callback) {
 
-addon.NodeIbapi.prototype.doReqFunc = function () {
-  if (this.funcQueue[0] != null) {
-      (this.funcQueue.shift())();
-  } 
-}
+        var messages = this.processMessage();
 
-addon.NodeIbapi.prototype.processIbMsg = function () {
-  this.checkMessages();
-  this.processMsg();
+        async.eachSeries(_.keys(messages), function (key, cb) {
 
-  retTickPrice = this.getTickPrice();
-  retTickSize = this.getTickSize();
-  retTickOptionComputation = this.getTickOptionComputation();
-  retTickGeneric = this.getTickGeneric();
-  retTickString = this.getTickString();
-  retTickEFP = this.getTickEFP();
-  retOrderStatus = this.getOrderStatus();
-  retOpenOrder = this.getOpenOrder();
-  retOpenOrderEnd = this.getOpenOrderEnd();
-  retWinError = this.getWinError();
-  retConnectionClosed = this.getConnectionClosed();
-  retUpdateAccountValue = this.getUpdateAccountValue();
-  retUpdatePortfolio = this.getUpdatePortfolio();
-  retUpdateAccountTime = this.getUpdateAccountTime();
-  retAccountDownloadEnd = this.getAccountDownloadEnd();
-  retNextValidId = this.getNextValidId();
-  retContractDetails = this.getContractDetails();
-  retBondContractDetails = this.getBondContractDetails();
-  retContractDetailsEnd = this.getContractDetailsEnd();
-  retExecDetails = this.getExecDetails();
-  retExecDetailsEnd = this.getExecDetailsEnd();
-  retError = this.getError();
-  retUpdateMktDepth = this.getUpdateMktDepth();
-  retUpdateMktDepthL2 = this.getUpdateMktDepthL2();
-  retUpdateNewsBulletin = this.getUpdateNewsBulletin();
-  retManagedAccounts = this.getManagedAccounts();
-  retReceiveFA = this.getReceiveFA();
-  retHistoricalData = this.getHistoricalData();
-  retScannerParameters = this.getScannerParameters();
-  retScannerData = this.getScannerData();
-  retScannerDataEnd = this.getScannerDataEnd();
-  retRealtimeBar = this.getRealtimeBar();
-  retFundamentalData = this.getFundamentalData();
-  retDeltaNeutralValidation = this.getDeltaNeutralValidation();
-  retTickSnapshotEnd = this.getTickSnapshotEnd();
-  retMarketDataType = this.getMarketDataType();
-  retCommissionReport = this.getCommissionReport();
-  retPosition = this.getPosition();
-  retPositionEnd = this.getPositionEnd();
-  retAccountSummary = this.getAccountSummary();
-  retAccountSummaryEnd = this.getAccountSummaryEnd();
-  retVerifyMessageAPI = this.getVerifyMessageAPI();
-  retVerifyCompleted = this.getVerifyCompleted();
-  retDisplayGroupList = this.getDisplayGroupList();
-  retDisplayGroupUpdated = this.getDisplayGroupUpdated();
+            if (key in this.handlers) {
+                var handler = this.handlers[key];
 
-  if (retTickPrice.isValid) {
-    this.emit('tickPrice', retTickPrice);
-  }
-  if (retTickSize.isValid) {
-    this.emit('tickSize', retTickSize);
-  }
-  if (retTickOptionComputation.isValid) {
-    this.emit('tickOptionComputation', retTickOptionComputation);
-  }
-  if (retTickGeneric.isValid) {
-    this.emit('tickGeneric', retTickGeneric);
-  }
-  if (retTickString.isValid) {
-    this.emit('tickString', retTickString);
-  }
-  if (retTickEFP.isValid) {
-    this.emit('tickEFP', retTickEFP);
-  }
-  if (retOrderStatus.isValid) {
-    this.emit('orderStatus', retOrderStatus);
-  }
-  if (retOpenOrder.isValid) {
-    this.emit('openOrder', retOpenOrder);
-  }
-  if (retOpenOrderEnd.isValid) {
-    this.emit('openOrderEnd', retOpenOrderEnd);
-  }
-  if (retWinError.isValid) {
-    this.emit('clientError', retWinError);
-  }
-  if (retConnectionClosed.isValid) {
-    this.emit('connectionClosed', retConnectionClosed);
-  }
-  if (retUpdateAccountValue.isValid) {
-    this.emit('updateAccountValue', retUpdateAccountValue);
-  }
-  if (retUpdatePortfolio.isValid) {
-    this.emit('updatePortfolio', retUpdatePortfolio);
-  }
-  if (retUpdateAccountTime.isValid) {
-    this.emit('updateAccountTime', retUpdateAccountTime);
-  }
-  if (retAccountDownloadEnd.isValid) {
-    this.emit('accountDownloadEnd', retAccountDownloadEnd);
-  }
-  if (retNextValidId.isValid) {
-    this.emit('nextValidId', retNextValidId);
-  }
-  if (retContractDetails.isValid) {
-    this.emit('contractDetails', retContractDetails);
-  }
-  if (retBondContractDetails.isValid) {
-    this.emit('bondContractDetails', retBondContractDetails);
-  }
-  if (retContractDetailsEnd.isValid) {
-    this.emit('contractDetailsEnd', retContractDetailsEnd);
-  }
-  if (retExecDetails.isValid) {
-    this.emit('execDetails', retExecDetails);
-  }
-  if (retExecDetailsEnd.isValid) {
-    this.emit('execDetailsEnd', retExecDetailsEnd);
-  }
-  if (retError.isValid) {
-    this.emit('svrError', retError);
-  }
-  if (retUpdateMktDepth.isValid) {
-    this.emit('updateMktDepth', retUpdateMktDepth);
-  }
-  if (retUpdateMktDepthL2.isValid) {
-    this.emit('updateMktDepthL2', retUpdateMktDepthL2);
-  }
-  if (retUpdateNewsBulletin.isValid) {
-    this.emit('updateNewsBulletin', retUpdateNewsBulletin);
-  }
-  if (retManagedAccounts.isValid) {
-    this.emit('managedAccounts', retManagedAccounts);
-  }
-  if (retReceiveFA.isValid) {
-    this.emit('receiveFA', retReceiveFA);
-  }
-  if (retHistoricalData.isValid) {
-    this.emit('historicalData', retHistoricalData);
-  }
-  if (retScannerParameters.isValid) {
-    this.emit('scannerParameters', retScannerParameters);
-  }
-  if (retScannerData.isValid) {
-    this.emit('scannerData', retScannerData);
-  }
-  if (retScannerDataEnd.isValid) {
-    this.emit('scannerDataEnd', retScannerDataEnd);
-  }
-  if (retRealtimeBar.isValid) {
-    this.emit('realtimeBar', retRealtimeBar);
-  }
-  if (retFundamentalData.isValid) {
-    this.emit('fundamentalData', retFundamentalData);
-  }
-  if (retDeltaNeutralValidation.isValid) {
-    this.emit('deltaNeutralValidation', retDeltaNeutralValidation);
-  }
-  if (retTickSnapshotEnd.isValid) {
-    this.emit('tickSnapshotEnd', retTickSnapshotEnd);
-  }
-  if (retMarketDataType.isValid) {
-    this.emit('marketDataType', retMarketDataType);
-  }
-  if (retCommissionReport.isValid) {
-    this.emit('commissionReport', retCommissionReport);
-  }
-  if (retPosition.isValid) {
-    this.emit('position', retPosition);
-  }
-  if (retPositionEnd.isValid) {
-    this.emit('positionEnd', retPositionEnd);
-  }
-  if (retAccountSummary.isValid) {
-    this.emit('accountSummary', retAccountSummary);
-  }
-  if (retAccountSummaryEnd.isValid) {
-    this.emit('accountSummaryEnd', retAccountSummaryEnd);
-  }
-  if (retNextValidId.isValid) {
-    this.emit('nextValidId', retNextValidId);
-  }
-  if (retVerifyMessageAPI.isValid) {
-    this.emit('verifyMessageAPI', retVerifyMessageAPI);
-  }
-  if (retVerifyCompleted.isValid) {
-    this.emit('verifyCompleted', retVerifyCompleted);
-  }
-  if (retDisplayGroupList.isValid) {
-    this.emit('displayGroupList', retDisplayGroupList);
-  }
-  if (retDisplayGroupUpdated.isValid) {
-    this.emit('displayGroupUpdated', retDisplayGroupUpdated);
-  }
+                var message = messages[key];
 
-  if (!this.isConnected())
-    this.emit('disconnected');
-}
+                handler(message, cb);
+            }
 
-addon.NodeIbapi.prototype.connectToIb = function (host,port,clientId) {
-  if (this.connect(host,port,clientId))
-    this.emit('connected');
-  else
-    this.emit('connectionFail');
-}
+        }.bind(this), callback);
+
+    },
+
+    processMessage: function () {
+        var messages = {};
+        this.checkMessages();
+        this.processMsg();
+
+        function checkMessage(messageId, objectData) {
+            if (objectData.isValid) {
+                messages[messageId] = objectData;
+            }
+        }
+
+        checkMessage(messageIds.tickPrice, this.getTickPrice());
+        checkMessage(messageIds.tickSize, this.getTickSize());
+        checkMessage(messageIds.tickOptionComputation, this.getTickOptionComputation());
+        checkMessage(messageIds.tickGeneric, this.getTickGeneric());
+        checkMessage(messageIds.tickString, this.getTickString());
+        checkMessage(messageIds.tickEFP, this.getTickEFP());
+        checkMessage(messageIds.orderStatus, this.getOrderStatus());
+        checkMessage(messageIds.openOrder, this.getOpenOrder());
+        checkMessage(messageIds.openOrderEnd, this.getOpenOrderEnd());
+        checkMessage(messageIds.clientError, this.getWinError());
+        checkMessage(messageIds.connectionClosed, this.getConnectionClosed());
+        checkMessage(messageIds.updateAccountValue, this.getUpdateAccountValue());
+        checkMessage(messageIds.updatePortfolio, this.getUpdatePortfolio());
+        checkMessage(messageIds.updateAccountTime, this.getUpdateAccountTime());
+        checkMessage(messageIds.accountDownloadEnd, this.getAccountDownloadEnd());
+        checkMessage(messageIds.nextValidId, this.getNextValidId());
+        checkMessage(messageIds.contractDetails, this.getContractDetails());
+        checkMessage(messageIds.retBondContractDetails, this.getBondContractDetails());
+        checkMessage(messageIds.contractDetailsEnd, this.getContractDetailsEnd());
+        checkMessage(messageIds.execDetails, this.getExecDetails());
+        checkMessage(messageIds.execDetailsEnd, this.getExecDetailsEnd());
+        checkMessage(messageIds.svrError, this.getError());
+        checkMessage(messageIds.updateMktDepth, this.getUpdateMktDepth());
+        checkMessage(messageIds.updateMktDepthL2, this.getUpdateMktDepthL2());
+        checkMessage(messageIds.updateNewsBulletin, this.getUpdateNewsBulletin());
+        checkMessage(messageIds.managedAccounts, this.getManagedAccounts());
+        checkMessage(messageIds.receiveFA, this.getReceiveFA());
+        checkMessage(messageIds.historicalData, this.getHistoricalData());
+        checkMessage(messageIds.scannerParameters, this.getScannerParameters());
+        checkMessage(messageIds.scannerData, this.getScannerData());
+        checkMessage(messageIds.scannerDataEnd, this.getScannerDataEnd());
+        checkMessage(messageIds.realtimeBar, this.getRealtimeBar());
+        checkMessage(messageIds.fundamentalData, this.getFundamentalData());
+        checkMessage(messageIds.deltaNeutralValidation, this.getDeltaNeutralValidation());
+        checkMessage(messageIds.tickSnapshotEnd, this.getTickSnapshotEnd());
+        checkMessage(messageIds.marketDataType, this.getMarketDataType());
+        checkMessage(messageIds.commissionReport, this.getCommissionReport());
+        checkMessage(messageIds.position, this.getPosition());
+        checkMessage(messageIds.positionEnd, this.getPositionEnd());
+        checkMessage(messageIds.accountSummary, this.getAccountSummary());
+        checkMessage(messageIds.accountSummaryEnd, this.getAccountSummaryEnd());
+        checkMessage(messageIds.verifyMessageAPI, this.getVerifyMessageAPI());
+        checkMessage(messageIds.verifyCompleted, this.getVerifyCompleted());
+        checkMessage(messageIds.displayGroupList, this.getDisplayGroupList());
+        checkMessage(messageIds.displayGroupUpdated, this.getDisplayGroupUpdated());
+
+        if (!this.isConnected()) {
+            messages[messageIds.disconnected] = {};
+        }
+
+        return messages;
+    },
+
+    connect: function (host, port, clientId) {
+        return this.client.connect(host, port, clientId)
+    },
+
+    beginProcessing: function () {
+        this._messageQueue.resume()
+    },
+
+    addReqId: function () {
+        this.client.reqIds(1);
+    }
+
+};
 
 exports = module.exports = {
-  "addon": addon,
-  "contract": contract,
-  "execution": execution,
-  "scannerSubscription": scannerSubscription,
-  "contractDetails": contractDetails,
-  "order": order
+    "NodeIBApi": NodeIBApi,
+    "messageIds": messageIds,
+    "contract": contract,
+    "execution": execution,
+    "scannerSubscription": scannerSubscription,
+    "contractDetails": contractDetails,
+    "order": order
 };
