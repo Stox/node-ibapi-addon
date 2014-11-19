a104dfbc55fd8867cbebbdb2f8f208eb98cc4d69
amitmiran <amitmiran@gmail.com>
2014-11-19 13:49:15 +0200

    market data of forex instruments

--- examples/mktDataSub.js
+++ examples/mktDataSub.js
@@ -4,71 +4,103 @@
 // Also, in this example we will submit a market data subscription 
 //  to MSFT.
 
-var ibapi = require('../ibapi');
-var ibcontract = ibapi.contract;
-var client = new ibapi.addon.NodeIbapi();
+var addon = require('../ibapi'),
+  messageIds = addon.messageIds,
+  contract = addon.contract,
+  order = addon.order;
 
-var processIbMsg = function () {
-  client.processIbMsg();
-}
-var doReqFunc = function () {
-  client.doReqFunc();
-}
-var disconnectClient = function () {
-  client.disconnect();
+var requestToInstrument={
+    1:"EURUSD",
+    2:"GBPUSD"
 }
 
-var eurusd = ibcontract.createContract();
+var eurusd = contract.createContract();
 eurusd.symbol = 'EUR';
 eurusd.secType = 'CASH';
 eurusd.exchange = 'IDEALPRO';
 eurusd.primaryExchange = 'IDEALPRO';
 eurusd.currency = 'USD';
 
-var msftContract = ibcontract.createContract();
+var gbpusd = contract.createContract();
+gbpusd.symbol = 'GBP';
+gbpusd.secType = 'CASH';
+gbpusd.exchange = 'IDEALPRO';
+gbpusd.primaryExchange = 'IDEALPRO';
+gbpusd.currency = 'USD';
+
+var msftContract = contract.createContract();
 msftContract.symbol = 'MSFT';
 msftContract.secType = 'STK';
 msftContract.exchange = 'SMART';
 msftContract.primaryExchange = 'NASDAQ';
 msftContract.currency = 'USD';
 
-var subscribeEurUsd = function () {
-  client.reqMktData(1, eurusd, "165", false);
-}
-var subscribeMsft = function () {
-  client.reqMktData(3, msftContract, "165", false);
-}
+var api = new addon.NodeIBApi();
 
-client.on('connected', function () {
-  console.log('connected');
-  setInterval(processIbMsg, 0.1);
-  client.funcQueue.push(subscribeEurUsd);
-  client.funcQueue.push(subscribeMsft);
-})
-  .once('nextValidId', function (data) {
-    orderId = data.orderId;
-    setInterval(doReqFunc, 100);
-    setTimeout(disconnectClient, 9001);
-  })
-  .on('tickPrice', function (tickPrice) {
-    console.log("TickPrice: " + tickPrice.tickerId.toString() + " " +
-                tickPrice.field.toString() + " " + tickPrice.price.toString() + " " +
-                tickPrice.canAutoExecute.toString());
-  })
-  .on('tickSize', function (tickSize) {
-    console.log("TickSize: " + tickSize.tickerId.toString() + " " +
-                tickSize.field.toString() + " " + tickSize.size.toString());
-  })
-  .on('clientError', function (clientError) {
-    console.log('Client error' + clientError.id.toString());
-  })
-  .on('svrError', function (svrError) {
-    console.log('Error: ' + svrError.id.toString() + ' - ' +
-                svrError.errorCode.toString() + ' - ' + svrError.errorString.toString());
-  })
-  .on('disconnected', function () {
-    console.log('disconnected');
-    process.exit(1);
-  })
+var handleValidOrderId = function (message, callback) {
+  orderId = message.orderId;
+  console.log('next order Id is ' + orderId);
+  callback()
 
-client.connectToIb('127.0.0.1', 7496, 0);
+};
+var handleServerError = function (message, callback) {
+  console.log('Error: ' + message.id.toString() + ' - ' +
+              message.errorCode.toString() + ' - ' + message.errorString.toString());
+  callback()
+};
+
+var tickTypes={
+    1:"bid",
+    2:"ask",
+    4:"last",
+    6:"high",
+    7:"low",
+    9:"close"
+};
+
+var handleTickPrice = function (tickPrice, callback) {
+  console.log("TickPrice: " + requestToInstrument[tickPrice.tickerId]+ " " +
+              tickTypes[tickPrice.field] + " " + tickPrice.price.toString() + " " +
+              tickPrice.canAutoExecute.toString());
+  callback()
+};
+var handleTickSize = function (tickSize, callback) {
+  console.log("TickSize: " + tickSize.tickerId.toString() + " " +
+              tickSize.field.toString() + " " + tickSize.size.toString());
+  callback()
+};
+var handleExecutions = function (message, callback) {
+  console.log('execDetails');
+  console.log(JSON.stringify(message));
+  callback()
+};
+
+var handleClientError = function (message, callback) {
+  console.log('clientError');
+  console.log(JSON.stringify(message));
+  callback()
+};
+
+var handleDisconnected = function (message, callback) {
+  console.log('disconnected');
+  callback();
+  process.exit(1);
+};
+
+api.handlers[messageIds.nextValidId] = handleValidOrderId;
+api.handlers[messageIds.svrError] = handleServerError;
+api.handlers[messageIds.tickPrice] = handleTickPrice;
+//api.handlers[messageIds.tickSize] = handleTickSize;
+api.handlers[messageIds.execDetails] = handleExecutions;
+api.handlers[messageIds.clientError] = handleClientError;
+api.handlers[messageIds.disconnected] = handleDisconnected;
+
+var connected = api.connect('127.0.0.1', 7496, 0);
+
+if (connected) {
+  api.beginProcessing();
+  setTimeout(api.reqMktData.bind(api,1, eurusd, "165", false), 300);
+  setTimeout(api.reqMktData.bind(api,2, gbpusd, "165", false), 300);
+  //setTimeout(api.reqMktData.bind(api,3, msftContract, "165", false), 300)
+  //setTimeout(api.disconnect.bind(api),20000)
+}
